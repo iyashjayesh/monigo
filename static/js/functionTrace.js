a5ae9901b7e19569bb8e34f5bb1fc8095677104b
Yash Chauhan <er.yashjayeshchauhan@gmail.com>
2024-09-16 15:43:33 +0530

    [Updated] html files with minor changes

--- static/js/functionTrace.js
+++ static/js/functionTrace.js
@@ -16,6 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     Object.values(uiElements).forEach(el => el && (el.innerHTML = loadingHtml));
+
     function fetchAndDisplayFunctionMetrics() {
         fetch(`/monigo/api/v1/function`)
             .then(response => response.json())
@@ -70,12 +71,20 @@ document.addEventListener('DOMContentLoaded', () => {
                 <div class="modal-dialog modal-xl modal-dialog-scrollable" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="functionDetailModalTitle">Details for ${funcName}</h5>
+                            <h5 class="modal-title" id="functionDetailModalTitle">Details for "${funcName}"</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <div class="modal-body">
+                            <div class="form-group">
+                                <label for="reportTypeSelect">Select Type</label>
+                                <div id="reportTypeButtons" class="btn-group ml-3" role="group">
+                                    <button type="button" class="btn btn-outline-primary report-type-btn active" data-bs-toggle="tooltip" title="Prints the location entries, one per line, including the flat and cum values. For more info check 'pprof'" data-report-type="text" id="btn-text">Text<span style="color:red; font-size: 18px; vertical-align: top;">*</span></button>
+                                    <button type="button" class="btn btn-outline-primary report-type-btn" data-bs-toggle="tooltip" title="Prints each location entry with its predecessors and successors. For more info check 'pprof'" data-report-type="traces" id="btn-traces">Traces<span style="color:red; font-size: 18px; vertical-align: top;">*</span></button>
+                                    <button type="button" class="btn btn-outline-primary report-type-btn" data-bs-toggle="tooltip" title="Prints each sample with a location per line. For more info check 'pprof'" data-report-type="tree" id="btn-tree">Tree<span style="color:red; font-size: 18px; vertical-align: top;">*</span></button>
+                                </div>
+                            </div>
                             <div id="function-details-content">Loading details...</div>
                         </div>
                         <div class="modal-footer">
@@ -89,23 +98,43 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.insertAdjacentHTML('beforeend', modalHtml);
         const modalElement = new bootstrap.Modal(document.getElementById('functionDetailModal'));
         modalElement.show();
-        fetch(`/monigo/api/v1/function-details?name=${funcName}&reportType=text`)
-            .then(response => response.json())
-            .then(details => {
-                const content = `
-                    <h5>Code Trace</h5>
-                    <pre>${details.function_code_trace || 'No code trace data available.'}</pre>
-                    <h5>Core Profile</h5>
-                    <pre>${details.core_profile.cpu_profile || 'No core profile data available.'}</pre>
-                    <h5>Memory Profile</h5>
-                    <pre>${details.core_profile.mem_profile || 'No memory profile data available.'}</pre>
-                `;
-                document.getElementById('function-details-content').innerHTML = content;
-            })
-            .catch(error => {
-                console.error('Error fetching function details:', error);
-                document.getElementById('function-details-content').innerHTML = "Error loading function details. Please try again later.";
+        
+        const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+        tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+            new bootstrap.Tooltip(tooltipTriggerEl);
+        });
+
+        const fetchFunctionDetails = (reportType) => {
+            fetch(`/monigo/api/v1/function-details?name=${funcName}&reportType=${reportType}`)
+                .then(response => response.json())
+                .then(details => {
+                    const content = `
+                        <h5>Code Trace</h5>
+                        <pre>${details.function_code_trace || 'No code trace data available.'}</pre>
+                        <h5>Core Profile</h5>
+                        <pre>${details.core_profile.cpu_profile || 'No core profile data available.'}</pre>
+                        <h5>Memory Profile</h5>
+                        <pre>${details.core_profile.mem_profile || 'No memory profile data available.'}</pre>
+                    `;
+                    document.getElementById('function-details-content').innerHTML = content;
+                })
+                .catch(error => {
+                    console.error('Error fetching function details:', error);
+                    document.getElementById('function-details-content').innerHTML = "Error loading function details. Please try again later.";
+                });
+        };
+
+        fetchFunctionDetails('text');
+        document.querySelectorAll('.report-type-btn').forEach(button => {
+            button.addEventListener('click', (event) => {
+                const selectedReportType = event.target.getAttribute('data-report-type');
+                fetchFunctionDetails(selectedReportType);
+
+                // Remove active class from all buttons and set the active class to the clicked one
+                document.querySelectorAll('.report-type-btn').forEach(btn => btn.classList.remove('active'));
+                event.target.classList.add('active');
             });
+        });
 
         document.getElementById('functionDetailModal').addEventListener('hidden.bs.modal', function () {
             document.getElementById('functionDetailModal').remove();
